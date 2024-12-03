52abad875cb165f123205f88ea9725e9ee951498
Khattab Khatir <khatab.najeeb88@gmail.com>
2024-12-03 21:30:44 +0300

    Handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,7 +67,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         const client = await sql.connect();
         await client.sql`DELETE
