dbb5cb6464ff335c2a768b5d1f731396a2de2524
Khattab Khatir <khatab.najeeb88@gmail.com>
2024-12-04 16:26:54 +0300

    1- Login 2- speed monitoring 3- Meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+      {children}
+      <SpeedInsights />
+      </body>
     </html>
   );
 }
